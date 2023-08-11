94d6a5a1337d3fe27704b3cdbf97abad173d1e5f
Lnsynd <2034215871@qq.com>
2023-08-11 14:12:43 +0800

    登录页面和请求发送

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+<div style="width: 100vw;height: 100vh;overflow: hidden;display: flex;">
+  <div style="flex: 1;background-color:#46dc5f;">
+    <el-image style="width: 100%;height: 100%" fit="cover"
+              src="src/img/welcome.jpeg"/>
+  </div>
+  <div class="welcome-title">
+    <div style="font-size: 30px;font-weight: bold;">欢迎来到我的网站！</div>
+    <div style="float: right;font-size: 18px;">-----一个拉米-阿弥诺斯</div>
+  </div>
+  <div class="right-card">
+    <router-view/>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+.right-card{
+  width: 400px;
+  z-index: 1;
+  background-color: #ffffff;
+}
+</style>
